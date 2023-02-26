706ee9a560dc615c30db068a966e2d779f411d53
icaro.albar@darede.com.br <icaro.albar@darede.com.br>
2023-02-26 18:39:40 -0300

    update name company telegram

--- controllers/sendController.js
+++ controllers/sendController.js
@@ -14,10 +14,10 @@ module.exports = class sendController {
     static async telegram(req, res) {
 
         const bot = new Telegraf(process.env.BOT_TOKEN);
-        const { id, name, email, phone, subject, message } = req.body
+        const { id, company, name, email, phone, subject, message } = req.body
 
         bot.telegram.sendMessage(process.env.BOT_USER, `  
-            Menssagem do site AGÁ Empreendimentos
+            Menssagem do site ${company}
         
             ID:  ${id}
             
@@ -43,19 +43,19 @@ module.exports = class sendController {
 
     static async sendMail(req, res) {
 
-        const { id, name, email, phone, subject, message } = req.body
+        const { id, image, company, site, name, email, phone, subject, message } = req.body
 
         transporter.sendMail({
-            from: process.env.SES_FROM,
+            from: `Contato ${company} ${process.env.SES_FROM}`,
             to: process.env.SES_TO,
-            subject: process.env.SES_SUBJECT,
+            subject: `${process.env.SES_SUBJECT} ${company}`,
             text: "Hello world?",
             html: `<style>*{font-family:arial,sans-serif}a{text-decoration:none;color:#000}th,td{padding:8px}span{font-weight:800;padding-right:5px}h4,p{text-align:center}.logo{padding-bottom:10px;border-bottom:solid 4px #d3ae58}</style>
                     <div class="logo">
-                    <img src="https://www.hpcap.com.br/logoAga.svg" alt="Logo AGÁ Empreendimentos" width="100">
+                    <img src="https://www.${process.env.SES_IMAGE_URL}.com.br/logo${image}.svg" alt="Logo ${company}" width="100">
                     </div>
                     <div>
-                    <h2>Menssagem do site AGÁ Empreendimentos</h2>
+                    <h2>Menssagem do site ${company}</h2>
                     <table>
                     <tr>
                     <td><span>ID:</span>${id}</td>
@@ -82,7 +82,7 @@ module.exports = class sendController {
                     </div>
                     <div>
                     <p>Em breve, voltaremos com mais informativos do site.</p>
-                    <h4><a href="https://www.agaempreendimentos.com.br/">AGÁ Empreendimentos</a></h4>
+                    <h4><a href="https://www.${site}.com.br/">${company}</a></h4>
                     </div>
                 `,
         })
@@ -95,7 +95,7 @@ module.exports = class sendController {
 
     static async database(req, res) {
 
-        const { id, name, email, phone, subject, message } = req.body
+        const { id, company, name, email, phone, subject, message } = req.body
         const diaAtual = format(new Date(), 'dd/MM/yyyy', {
             locale: ptBR
         })
@@ -106,6 +106,7 @@ module.exports = class sendController {
             Item: {
                 'DATA': { S: diaAtual },
                 'ID': { S: id },
+                "EMPRESA": { S: company },
                 'NOME': { S: name },
                 'EMAIL': { S: email },
                 'TELEFONE': { S: phone },
